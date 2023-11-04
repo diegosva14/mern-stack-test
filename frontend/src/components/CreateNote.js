7644e52ff2ef61e0c2ebb7d5a2549993501e2cf8
diegosva14 <diegosotovalle14@gmail.com>
2023-11-03 21:07:27 -0700

    second commit

--- frontend/src/components/CreateNote.js
+++ frontend/src/components/CreateNote.js
@@ -51,9 +51,9 @@ export default class CreateNote extends Component {
             author: this.state.userSelected
         }
         if(this.state.editing){
-            await axios.put(URI_NOTE + this.state._id, newNote)
+            await axios.put('https://mern-stack-test-frontend-qjct.onrender.com/api/notes' + this.state._id, newNote)
         }else{
-            await axios.post(URI_NOTE, newNote);
+            await axios.post('https://mern-stack-test-frontend-qjct.onrender.com/api/notes', newNote);
         }
 
 
