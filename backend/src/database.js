2784bbb5f37b834dfe14e72dd911e00972ddea53
diegosva14 <diegosotovalle14@gmail.com>
2023-11-03 21:13:57 -0700

    third commit

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,12 +1,19 @@
 
 const mongoose = require('mongoose');
 
-const uri = 'mongodb+srv://quinonezcesar:vrZtCSbhMbTGV6gH@cluster0.uoiyt8n.mongodb.net/?retryWrites=true&w=majority';
-mongoose.connect(uri).then(db => console.log('db is conected')).catch(err => console.log(err));
+const URI = process.env.MONGODB_URI
+    ? process.env.MONGODB_URI
+    : 'mongodb://localhost/databasetest';
 
+mongoose.connect(URI,{
+    useNewUrlParser: true,
+    //useUnifiedTopology: true,
+    
+});
 
-module.exports = mongoose;
+//mongoose.connect(URI).then(db => console.log('db is conected')).catch(err => console.log(err));
 
-
-//quinonezcesar
-//vrZtCSbhMbTGV6gH
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('DB is connected');
+});
