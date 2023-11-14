af4233a6e09a43295dcecfb650cad92d576dc9f9
diegosva14 <diegosotovalle14@gmail.com>
2023-11-13 22:37:57 -0800

    commited

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 export default class NotesList extends Component {
 
     render(){
