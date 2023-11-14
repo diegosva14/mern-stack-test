f3169b407caf0d1f97c475071d52d5a9e58050e7
diegosva14 <diegosotovalle14@gmail.com>
2023-11-13 22:21:09 -0800

    commited

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,9 +11,10 @@ import SignUp from './components/SignUp';
 function App() {
   return (
     <Router>
+    <SignUp/> 
     <Navigation/>
     <div className="container p-4">
-    <Route path="/signup" component={SignUp} />
+    <Route path="/signup" component={SignUp} /> 
       <Route path="/" exact component={NotesList} />
       <Route path={"/edit/:id"} component={CreateNote} />
       <Route path="/create" component={CreateNote} />
