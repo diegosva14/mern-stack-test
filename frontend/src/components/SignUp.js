e17f864bb2e7a16beaade2eb245b0566069e844e
diegosva14 <diegosotovalle14@gmail.com>
2023-11-14 01:18:35 -0800

    commited

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom'
 export default class SignUp extends Component {
 
     render(){
